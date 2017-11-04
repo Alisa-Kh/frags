fbe2979c346dedf95ee1e7b15a2044aadc0f1bec
Alisa-Kh <33228670+Alisa-Kh@users.noreply.github.com>
2017-11-04 14:09:45 +0300

    Update pdb_from_frags.py

--- pdb_from_frags.py
+++ pdb_from_frags.py
@@ -8,30 +8,40 @@ parameters_sets = []
 frags_parameters = open('frags_parameters', 'w+')
 with open(frags_file_path) as frags_file:
     all_file_lines = frags_file.readlines()
-    j = 0
-    for i in range(2, len(all_file_lines)):
-        if j % 6 == 0:
-            first_line_in_set = all_file_lines[i]
-            last_line_in_set = all_file_lines[i+4]
 
-            # Get parameters from first line and last line and save them in values_sets
-            first_line_words = first_line_in_set.split()
-            last_line_words = last_line_in_set.split()
+# Count a number of lines in one block (one fragment)
+first_blank_line = None
+second_blank_line = None
+for i in range(len(all_file_lines)):
+    if all_file_lines[i] in ['\n', '\r\n']:
+        if first_blank_line is None:
+            first_blank_line = i
+        elif second_blank_line is None:
+            second_blank_line = i
+            break
 
-            pdb = first_line_words[0]
-            chain = first_line_words[1]
-            start_res = first_line_words[2]
-            end_res = last_line_words[2]
-            # write the
-            parameters_sets.append([pdb, chain, start_res, end_res])
-        j += 1
-        
-print(parameters_sets)
+lines_in_block_num = second_blank_line - first_blank_line
+# Get parameters from first line and last line and save them in values_sets
+j = 0
+for i in range(2, len(all_file_lines)):
+    if j % lines_in_block_num == 0:
+        first_line_in_set = all_file_lines[i]
+        last_line_in_set = all_file_lines[i+(lines_in_block_num-2)]
+        seq = ""
+        for k in range(i, i+lines_in_block_num-1):
+            line = all_file_lines[k].split()
+            seq += line[3]
+        first_line_words = first_line_in_set.split()
+        last_line_words = last_line_in_set.split()
+        pdb = first_line_words[0]
+        chain = first_line_words[1]
+        start_res = first_line_words[2]
+        end_res = last_line_words[2]
+        parameters_sets.append([pdb, chain, start_res, end_res, seq])
+    j += 1
 
 for item in parameters_sets:
-    frags_parameters.write(item, '\n')
-frags_parameters.readlines()
-print(frags_parameters)
+    for par in item:
+        frags_parameters.write("%s " % par)
+    frags_parameters.write("\n")
 frags_parameters.close()
-
-
