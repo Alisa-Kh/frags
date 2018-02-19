fd0a0977bea3d7c70c10f6298a44472c4ae7e4b8
Alisa Khramushin <33228670+Alisa-Kh@users.noreply.github.com>
2018-02-19 16:44:00 +0200

    Update run_excise_pdb.py

--- run_excise_pdb.py
+++ run_excise_pdb.py
@@ -1,11 +1,22 @@
-import subprocess
 import os
 import sys
 
-parameters_file_path = sys.argv[1] # Get filename as an argument to the script
-original_seq = sys.argv[2] # peptide_sequence file
+ROSETTA_DATABASE = '/vol/ek/share/rosetta/rosetta_src_2017.45.59812_bundle/main/database'
 
-rosetta_database = '/vol/ek/share/rosetta/rosetta_src_2017.45.59812_bundle/main/database'
+# Commands
+
+PRODY = '/vol/ek/share/csbw/tools/env2017/bin/prody fetch %s'
+RENUMBERING = \
+    '/vol/ek/share/rosetta/rosetta_src_2017.45.59812_bundle/tools/protein_tools/scripts/pdb_renumber.py {} {}'
+EXCISE_PDB = '/vol/ek/share/rosetta/pdbUtil/excisePdb_v2.pl {} {} {} {} {}'
+FIXBB = '/vol/ek/share/rosetta/rosetta_src_2017.52.59948_bundle/main/source/bin/fixbb.linuxgccrelease' \
+        ' -database %s -in:file:s %s -resfile resfile_tmp -ex1 -ex2 -use_input_sc' \
+        ' -scorefile design_score.sc -ignore_zero_occupancy false >>design.log'
+
+# Get filename as an argument to the script
+parameters_file_path = sys.argv[1]
+original_seq = sys.argv[2]  # peptide_sequence
+
 
 """Update PYTHONPATH to be /vol/ek/share/rosetta/rosetta_src_2017.45.59812_bundle/tools/protein_tools/"""
 
@@ -28,32 +39,82 @@ for frag in fragments:
     end_res.append(frag.split()[3])
     sequences.append(frag.split()[4])
 
+
+def delete_frag(fragment):
+        os.remove(fragment)
+        print("Wrong length fragment has been deleted")
+
 # Fetch PDBs with prody, call excisePdb and then delete the pdb file
 for pdb, chain, start, end, sequence in zip(pdbs, chains, start_res, end_res, sequences):
     outfile = pdb + '.' + chain + '.' + start + '.' + end + '.pdb'
     pdb_full = pdb + '.pdb'
 
-    subprocess.run(['/vol/ek/share/csbw/tools/env2017/bin/prody', 'fetch', pdb])
+    os.system(PRODY % pdb)
 
-    subprocess.run(['/vol/ek/Home/alisa/scripts/check_my_scripts/excisePdb_working.pl',
-                        pdb_full, chain, start, end, outfile])
+    os.system(EXCISE_PDB.format(pdb_full, chain, start, end, outfile))
 
     if not os.path.exists(outfile):
-        print("renumbering:")
         new_pdb = pdb_full
-        renumbering = \
-            '/vol/ek/share/rosetta/rosetta_src_2017.45.59812_bundle/tools/protein_tools/scripts/pdb_renumber.py %s %s' \
-                      % (pdb_full, new_pdb)
-        os.system(renumbering) # Renumber PDBs which don't start from 1
 
-        subprocess.run(['/vol/ek/Home/alisa/scripts/check_my_scripts/excisePdb_working.pl',
-                        new_pdb, chain, start, end, outfile])
+        print("renumbering:")
+        os.system(RENUMBERING.format(pdb_full, new_pdb))  # Renumber PDB if it doesn't start from 1
+
+        os.system(EXCISE_PDB.format(pdb_full, chain, start, end, outfile))
 
     os.remove(pdb_full)
 
+# check correctness of fragments
+
+    with open(outfile) as frag:
+        bad_pdb = False
+        residues = set()
+        cur_line = frag.readline().split()
+        while cur_line[0] != 'ATOM':  # find ATOM lines
+            cur_line = frag.readline().split()
+
+            # if there is no atoms...
+            if not cur_line:
+                bad_pdb = True
+                break
+        if bad_pdb:
+            delete_frag(outfile)
+            continue
+
+        cur_line = frag.readline()
+        while cur_line[22:27].strip() != start:
+            cur_line = frag.readline()
+
+            # if there is no start residue
+            if not cur_line:
+                bad_pdb = True
+                break
+        if bad_pdb:
+            delete_frag(outfile)
+            continue
+
+        cur_line = frag.readline()
+        while cur_line[22:27].strip() != end:
+            residues.add(cur_line[22:27])
+            cur_line = frag.readline()
+
+            # if there is no end residue
+            if not cur_line:
+                bad_pdb = True
+                break
+        if bad_pdb:
+            delete_frag(outfile)
+            continue
+
+        residues.add(cur_line[22:27])
+    if len(residues) != (int(end) - int(start) + 1):
+        delete_frag(outfile)
+        continue
+
 # Create resfile and run fixbb
-    resfile = open('resfile', 'w')
+    resfile = open('resfile_tmp', 'w')
     resfile.write('NATRO\nstart')
+    if chain == '_':
+        chain = 'A'
     for i, res in enumerate(sequence):
         if res == ori_seq[i]:
             resfile.write('\n' + str(i + int(start)) + ' ' + chain + ' NATRO')
@@ -62,8 +123,9 @@ for pdb, chain, start, end, sequence in zip(pdbs, chains, start_res, end_res, se
                           ' EX 1 EX 2')
     resfile.close()
 
-    #fixbb run
-    fixbb = '/vol/ek/share/rosetta/rosetta_src_2017.52.59948_bundle//main/source/bin/fixbb.linuxgccrelease' \
-            ' -database %s -in:file:s %s -resfile resfile -ex1 -ex2 -use_input_sc -scorefile design_score.sc' \
-            ' >design.log' % (rosetta_database, outfile)
-    os.system(fixbb)
+    # fixbb run
+    print("running fixbb design")
+    os.system(FIXBB % (ROSETTA_DATABASE, outfile))
+
+    os.remove('resfile_tmp')
+    os.remove(outfile)
